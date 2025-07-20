9b1cf3411842a3084250d3b5566ab9d3d32b3dd5
Vendetta Tools <toolsvendetta@gmail.com>
2025-07-20 07:46:49 +0000

    Puedes mejorar y optimizar el rendimiento del global css y tsilwind conf

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       gridTemplateColumns: {
         '15': 'repeat(15, minmax(0, 1fr))',
@@ -98,12 +105,18 @@ const config: Config = {
           from: { opacity: '0', transform: 'translateY(10px)' },
           to: { opacity: '1', transform: 'translateY(0)' },
         },
+        shimmer: {
+          '100%': {
+            transform: 'translateX(100%)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in': 'fade-in 0.5s ease-in-out',
         'fade-in-up': 'fade-in-up 0.5s ease-in-out',
+        shimmer: 'shimmer 1.5s infinite',
       },
     },
   },
